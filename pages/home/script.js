1061ec9178452ba918dc8ce07b3c5509cbd8741f
Jhonnatas Mendonça <103781364+JhonnatasMendonca@users.noreply.github.com>
2023-04-06 19:58:25 -0300

    Adicionado modal cliente

--- pages/home/script.js
+++ pages/home/script.js
@@ -3,6 +3,31 @@ document.addEventListener('DOMContentLoaded', function() {
     $('.carrossel').slick({
         dots: true,
         autoplay: true,
-        autoplaySpeed: 5000
+        autoplaySpeed: 3000
     });
+
+
+    //Ativa o modal
+
+    $("#entrar").click(function(){
+
+        $(".modal").show();   
+        $('#box-principal').css("background-color", "#0F270F");
+        $('.buttons').css("color", "gray");
+        $('#input-pesquisa').css("background-color", "gray");
+        $('.icon-header').css("display", "none");
+        $('#logo').css("display", "none");
+    });
+
+    //Fecha o modal
+
+    $(".img-fechar").click(function(){
+        $(".modal").hide();   
+        $('#box-principal').css("background-color", "#859479"); 
+        $('.buttons').css("color", "#FFFFFF");
+        $('#input-pesquisa').css("background-color", "#FFFFFF");
+        $('.icon-header').css("display", "block");
+        $('#logo').css("display", "block");
+    });
+
 });

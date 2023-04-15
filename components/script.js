37419c743f482456dc070aa99c6bd8dd1bef2462
Keylane Cardoso <keylanecardoso28@gmail.com>
2023-04-15 02:41:58 -0300

    Alteração de carrossel, e na pag. de estilo padrão

--- components/script.js
+++ components/script.js
@@ -5,21 +5,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
     $(".entrar, .Central-vendedor").click(function(){
         if ($(this).attr('class') == "Central-vendedor") {
-
+            $(".vendedor-modal").fadeIn();
             $(".vendedor").fadeIn();
         }else{
-            $(".fundo-modal").fadeIn();
+            $(".cliente-modal").fadeIn();
             $(".cliente").fadeIn();
 
         }
         $('body').css("overflow", "hidden");
-        // $('#box-principal').css("background-color", "#0F270F");
-        // $('.buttons a').css("color", "gray");
-        // $('.buttons').css("color", "gray");
-        // $('#input-pesquisa').css("background-color", "gray");
-        // $('.icon-header').css("display", "none");
-        // $('#logo').css("display", "none");
-
+        
     });
 
     //Fecha o modal
@@ -29,12 +23,7 @@ document.addEventListener('DOMContentLoaded', function() {
         $(".modal").fadeOut();
 
         $('body').css("overflow", "visible");   
-        $('#box-principal').css("background-color", "#859479"); 
-        $('.buttons a').css("color", "#FFFFFF");
-        $('.buttons').css("color", "#FFFFFF");
-        $('#input-pesquisa').css("background-color", "#FFFFFF");
-        $('.icon-header').css("display", "block");
-        $('#logo').css("display", "block");
+        
     });
 
 });
